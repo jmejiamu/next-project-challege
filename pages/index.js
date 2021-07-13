3a411d78906d7d2c8675ba7b1966b2089d702afa
Jose <jmejiamu@gmail.com>
2021-07-13 07:49:56 -0700

    added the todo input

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,35 @@
 
-import styles from '../styles/Home.module.css'
+import { Button } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import { useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
+}));
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1>this is a test</h1>
-    </div>
-  )
+    const classes = useStyles();
+    const [todos, setTodos] = useState();
+
+    return (
+        <div >
+            <h1>TODO</h1>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField
+                    id="outlined-basic"
+                    label="Add Todo"
+                    variant="outlined"
+                    value={todos}
+                    onChange={(e) => setTodos(e.target.value)} />
+
+            </form>
+            <Button variant='contained' color="primary" >Submit</Button>
+        </div>
+    )
 }
