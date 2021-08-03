6fc8af6e3e4e42d3d8339c1192a56a083da318de
Jose <jmejiamu@gmail.com>
2021-08-02 17:12:20 -0700

    added dinamyc route for auth

--- src/components/DarkTheme.js
+++ src/components/DarkTheme.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Switch from '@material-ui/core/Switch';
 
 const DarkTheme = (props) => {
+
+
     return (
         <>
             <Switch checked={props.checked} onChange={() => props.setDark(!props.checked)} />
